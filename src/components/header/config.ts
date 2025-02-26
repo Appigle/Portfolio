5b7e3d718e7d96b4ddf8e68954a92036bc990060
Chenley <chenley@qq.com>
2025-02-26 10:19:39 -0500

    feat: update github contributions grid

--- src/components/header/config.ts
+++ src/components/header/config.ts
@@ -1,46 +1,46 @@
-import { Link } from "@/types";
+import { Link } from '@/types';
 
 const links: Link[] = [
   {
     title: 'Home',
     href: '/',
-    thumbnail: '/assets/nav-link-previews/landing.png'
+    thumbnail: '/assets/nav-link-previews/landing.png',
   },
   {
     title: 'About',
     href: '/#about',
-    thumbnail: '/assets/nav-link-previews/about.png'
+    thumbnail: '/assets/nav-link-previews/about.png',
   },
   {
     title: 'Skills',
     href: '/#skills',
-    thumbnail: '/assets/nav-link-previews/skills.png'
+    thumbnail: '/assets/nav-link-previews/skills.png',
   },
   {
     title: 'Projects',
     href: '/#projects',
-    thumbnail: '/assets/nav-link-previews/projects.png'
+    thumbnail: '/assets/nav-link-previews/projects.png',
   },
-  // {
-  //   title: 'Skills',
-  //   href: '/skills',
-  //   thumbnail: '/assets/nav-link-previews/skills.png'
-  // },
   // {
   //   title: 'Testimonials',
   //   href: '/testimonials',
   //   thumbnail: '/assets/nav-link-previews/testimonials.png'
   // },
   {
+    title: 'Github Contributions',
+    href: '/#GithubContributions',
+    thumbnail: 'https://green-wall.leoku.dev/api/og/share/Appigle',
+  },
+  {
     title: 'Blogs',
-    href: '/blogs',
+    href: 'https://appigle.xiaopotato.top/',
     thumbnail: '/assets/nav-link-previews/blog.png',
   },
   {
     title: 'Contact',
     href: '/#contact',
-    thumbnail: '/assets/nav-link-previews/contact.png'
-  }
+    thumbnail: '/assets/nav-link-previews/contact.png',
+  },
 ];
 
 export { links };
--- src/components/sections/github_contributions.tsx
+++ src/components/sections/github_contributions.tsx
@@ -1,18 +1,54 @@
-import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 const GithubContributions = () => {
   return (
-    <div className="flex justify-center w-full relative">
-      <a href="http://www.github.com/Appigle">
-        <Image
+    <section
+      id="GithubContributions"
+      className="max-w-7xl mx-auto md:h-[55vh] relative"
+    >
+      <Link href={'#GithubContributions'}>
+        <h2
+          className={cn(
+            'bg-clip-text text-4xl text-center text-transparent md:text-7xl pt-16',
+            'bg-gradient-to-b from-black/80 to-black/50',
+            'dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-32'
+          )}
+        >
+          Contributions
+        </h2>
+      </Link>
+      <a href="http://www.github.com/Appigle" className="flex justify-center">
+        {/* <Image
           className="rounded-lg w-[100%] hover:scale-[1.05] transition-all hover:cursor-pointer"
           src="https://green-wall.leoku.dev/api/og/share/Appigle"
           alt="My contributions"
           width={1000}
           height={500}
-        />
+        /> */}
+
+        <picture>
+          <source
+            media="(prefers-color-scheme: light)"
+            srcSet="https://github.com/Appigle/Appigle/raw/output/github-contribution-grid-snake-dark.svg"
+          />
+          <source
+            media="(prefers-color-scheme: dark)"
+            srcSet="https://github.com/Appigle/Appigle/raw/output/github-contribution-grid-snake.svg"
+          />
+          <img
+            className="rounded-lg w-[100%] hover:scale-[1.05] transition-all hover:cursor-pointer hidden dark:block"
+            alt="github-snake"
+            src="https://github.com/Appigle/Appigle/raw/output/github-contribution-grid-snake.svg"
+          />
+          <img
+            className="rounded-lg w-[100%] hover:scale-[1.05] transition-all hover:cursor-pointer dark:hidden"
+            alt="github-snake"
+            src="https://github.com/Appigle/Appigle/raw/output/github-contribution-grid-snake-dark.svg"
+          />
+        </picture>
       </a>
-    </div>
+    </section>
   );
 };
 export default GithubContributions;
